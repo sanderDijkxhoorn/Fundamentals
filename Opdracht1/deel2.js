738fbddb91b110a636f79aff706752147b5825c3
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-02-15 10:53:43 +0100

    Deel 1 klaar

--- Opdracht1/deel2.js
+++ Opdracht1/deel2.js
@@ -33,45 +33,23 @@ const rl = readline.createInterface({
 rl.question("X: ", function (x) {
     rl.question("Y: ", function (y) {
         rl.question("Z: ", function (z) {
-            if (x > y && x > z) {
-                if (y > z) {
+            if (x >= y && x >= z) {
+                if (y >= z) {
                     console.log(z  + ", " + y + ", " + x);
-                    //console.log(x + ", " + y + ", " + z);
                 } else {
                     console.log(y + ", " + z + ", " + x);
-                    //console.log(x + ", " + z + ", " + y);
                 }
-            } else if (y > x && y > z) {
-                if (x > z) {
+            } else if (y >= x && y >= z) {
+                if (x >= z) {
                     console.log(z + ", " + x + ", " + y);
                 } else {
                     console.log(x + ", " + z + ", " + y);
                 }
-            } else if (z > x && z > y) {
-                if (x > y) {
+            } else if (z >= x && z >= y) {
+                if (x >= y) {
                     console.log(y + ", " + x + ", " + z);
                 } else {
                     console.log(x + ", " + y + ", " + z);
-                }
-            } else if (z == x) {
-                if(x > y) {
-                    console.log(y + ", " + x + ", " + z);
-                } else if (x == y) {
-                    console.log(y + ", " + x + ", " + z);
-                } else {
-                    console.log(z + ", " + x + ", " + y);
-                }
-            } else if (y == x) {
-                if(z > y) {
-                    console.log(y + ", " + x + ", " + z);
-                } else {
-                    console.log(z + ", " + x + ", " + y);
-                }
-            } else if (z == y) {
-                if(x > y) {
-                    console.log(y + ", " + x + ", " + z);
-                } else {
-                    console.log(x + ", " + z + ", " + y);
                 }
             }
             rl.close();
@@ -82,19 +60,3 @@ rl.question("X: ", function (x) {
 rl.on("close", function () {
     process.exit(0);
 });
-
-// If statement maar dan kleiner gemaakt variant
-// const readline = require("readline"),
-//     rl = readline.createInterface({
-//         input: process.stdin,
-//         output: process.stdout
-//     });
-// rl.question("X: ", function (o) {
-//     rl.question("Y: ", function (l) {
-//         rl.question("Z: ", function (e) {
-//             o > l && o > e ? l > e ? console.log(e + ", " + l + ", " + o) : console.log(l + ", " + e + ", " + o) : l > o && l > e ? o > e ? console.log(e + ", " + o + ", " + l) : console.log(o + ", " + e + ", " + l) : e > o && e > l ? o > l ? console.log(l + ", " + o + ", " + e) : console.log(o + ", " + l + ", " + e) : e == o ? o > l ? console.log(l + ", " + o + ", " + e) : o == l ? console.log(l + ", " + o + ", " + e) : console.log(e + ", " + o + ", " + l) : l == o ? e > l ? console.log(l + ", " + o + ", " + e) : console.log(e + ", " + o + ", " + l) : e == l && (o > l ? console.log(l + ", " + o + ", " + e) : console.log(o + ", " + e + ", " + l)), rl.close()
-//         })
-//     })
-// }), rl.on("close", function () {
-//     process.exit(0)
-// });
---
+++ Opdracht2/deel2.js
@@ -0,0 +1,14 @@
+for(i=1; i <= 10; i++){
+    console.log(i);
+    console.log(i * 2);
+    console.log(i * 3);
+    console.log(i * 4);
+    console.log(i * 5);
+    console.log(i * 6);
+    console.log(i * 7);
+    console.log(i * 8);
+    console.log(i * 9);
+    console.log(i * 10);
+    console.log(i * 11);
+    console.log(i * 12);
+}
